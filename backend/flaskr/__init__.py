5f534432f21224d1c9a5cd0677dbccafeccd36ea
Amador Cuenca <sphi02ac@gmail.com>
2020-03-02 10:53:47 -0300

    feat: Implement CORS

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -14,19 +14,32 @@ def create_app(test_config=None):
   setup_db(app)
   
   '''
-  @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
+  @Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
   '''
+  cors = CORS(app, resources={r"/api/*": {'origins': "*"}})
 
   '''
-  @TODO: Use the after_request decorator to set Access-Control-Allow
+  @Use the after_request decorator to set Access-Control-Allow
   '''
+  @app.after_request
+  def after_request(response):
+    response.headers.add('Access-Control-Allow-Headers', "Content-Type,Authorization,true")
+    response.headers.add('Access-Control-Allow-Methods', "GET,PATCH,POST,DELETE,OPTIONS")
+    
+    return response
 
   '''
   @TODO: 
   Create an endpoint to handle GET requests 
   for all available categories.
   '''
-
+  @app.route('/api/categories', methods=['GET'])
+  def get_all_categories():
+    categories = Category.query.all()
+    
+    return jsonify({
+      'categories': [category.format() for category in categories]
+    })
 
   '''
   @TODO: 
