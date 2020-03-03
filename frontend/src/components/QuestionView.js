6d6ce7909c1108844e02eab4150fb845dc7ae0f5
Amador Cuenca <sphi02ac@gmail.com>
2020-03-02 21:12:59 -0300

    feat: Add the get questions by category endpoint

--- frontend/src/components/QuestionView.js
+++ frontend/src/components/QuestionView.js
@@ -23,7 +23,7 @@ class QuestionView extends Component {
 
   getQuestions = () => {
     $.ajax({
-      url: `http://localhost:5000/api/questions?page=${this.state.page}`, //TODO: update request URL
+      url: `http://localhost:5000/api/questions?page=${this.state.page}`,
       type: "GET",
       success: (result) => {
         this.setState({
@@ -60,7 +60,7 @@ class QuestionView extends Component {
 
   getByCategory= (id) => {
     $.ajax({
-      url: `/categories/${id}/questions`, //TODO: update request URL
+      url: `http://localhost:5000/api/categories/${id}/questions`,
       type: "GET",
       success: (result) => {
         this.setState({
